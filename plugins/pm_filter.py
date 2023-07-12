774e013f9dafbce9cc1b1aa36601e31c6bcd664a
Hani-malhotra <134125563+Hani-malhotra@users.noreply.github.com>
2023-07-12 21:45:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -47,37 +47,37 @@ async def give_filter(client, message):
              if settings['filter_mode']:
                  manual = await manual_filters(client, message)
                  if manual == False:
-                 settings = await get_settings(message.chat.id)
-                 try:
-                    if settings['auto_ffilter']:
-		        #if settings['filter_mode']:
-                        await advance_filter(client, message)
-			  #else:
-			      #await auto_filter(client, message)
-                 except KeyError:
-                      grpid = await active_connection(str(message.from_user.id))
-                      await save_group_settings(grpid, 'auto_ffilter', True)
-                      settings = await get_settings(message.chat.id)
-                      if settings['auto_ffilter']:
-			 #if settings['filter_mode']:     
-                         await advance_filter(client, message)    
-			 #else:
-			     #await auto_filter(client, message)
-	    else:
-            #glob = await global_filters(client, message)
-            #if glob == False:		
-                manual = await manual_filters(client, message)
-                if manual == False:
                      settings = await get_settings(message.chat.id)
                      try:
-                         if settings['auto_ffilter']:
-                             #await auto_filter(client, message)
-                     except KeyError:
-                        grpid = await active_connection(str(message.from_user.id))
-                        await save_group_settings(grpid, 'auto_ffilter', True)
-                        settings = await get_settings(message.chat.id)
                         if settings['auto_ffilter']:
-                            await auto_filter(client, message)		    
+		            #if settings['filter_mode']:
+                            await advance_filter(client, message)
+			      #else:
+			          #await auto_filter(client, message)
+                     except KeyError:
+                          grpid = await active_connection(str(message.from_user.id))
+                          await save_group_settings(grpid, 'auto_ffilter', True)
+                          settings = await get_settings(message.chat.id)
+                          if settings['auto_ffilter']:
+			      #if settings['filter_mode']:     
+                             await advance_filter(client, message)    
+			     #else:
+			         #await auto_filter(client, message)
+	     else:
+            #glob = await global_filters(client, message)
+            #if glob == False:		
+                 manual = await manual_filters(client, message)
+                 if manual == False:
+                      settings = await get_settings(message.chat.id)
+                      try:
+                          if settings['auto_ffilter']:
+                             #await auto_filter(client, message)
+                      except KeyError:
+                         grpid = await active_connection(str(message.from_user.id))
+                         await save_group_settings(grpid, 'auto_ffilter', True)
+                         settings = await get_settings(message.chat.id)
+                         if settings['auto_ffilter']:
+                             await auto_filter(client, message)		    
 
 		    
     else: #a better logic to avoid repeated lines of code in auto_filter function

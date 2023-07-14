8e7a1b4cdd18a1993ce80684d59c959bd3fda7b6
Hani-malhotra <134125563+Hani-malhotra@users.noreply.github.com>
2023-07-14 22:50:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1953,8 +1953,9 @@ async def auto_filter(client, msg, spoll=False):
             await dlt.delete()	
     		
     else:
-	await dlt.delete()	
-        fuk = await message.reply_photo(photo=NOR_IMG, caption=cap, reply_markup=InlineKeyboardMarkup(btn))			    
+		
+        fuk = await message.reply_photo(photo=NOR_IMG, caption=cap, reply_markup=InlineKeyboardMarkup(btn))	
+	await dlt.delete()
         try:	
             if settings['auto_delete']:
                 await asyncio.sleep(600)
